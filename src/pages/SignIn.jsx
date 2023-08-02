6c2420be95c207c11364bcab7aad23997812c47d
JOBOYA <lena.boya26@gmail.com>
2023-08-02 09:25:20 +0200

    sign

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { TextBoxComponent } from '@syncfusion/ej2-react-inputs/textbox';
 import './SignIn.css';
+import axios from 'axios';
 
 const SignIn = () => {
   const [usernameValue, setUsernameValue] = useState('');
@@ -9,19 +10,45 @@ const SignIn = () => {
   // Handle username input change
   const handleUsernameInputChange = (event) => {
     setUsernameValue(event.target.value);
+    console.log('Username:', event.target.value);
   };
-
-  // Handle password input change
+  
   const handlePasswordInputChange = (event) => {
     setPasswordValue(event.target.value);
+    console.log('Password:', event.target.value);
   };
+  
 
   // Handle sign in button click
-  const handleSignIn = () => {
-    // Insert authentication logic here
-    // eslint-disable-next-line no-console
-    console.log('Username: , Password: ');
-  };
+  // Handle sign in button click
+// Handle sign in button click
+const handleSignIn = async () => {
+  console.log('Signing in with:', usernameValue, passwordValue);
+  try {
+    const response = await axios.post('http://localhost:3000/login', {
+      username: usernameValue,
+      password: passwordValue,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true,
+    });
+    
+    if (response.status === 200) {
+      // Store the access token somewhere (e.g. localStorage)
+      localStorage.setItem('accessToken', response.data.accessToken);
+      localStorage.setItem('refreshToken', response.data.refreshToken);
+      // Do something after successful login (e.g. redirect to a different page)
+    } else {
+      // Handle error (e.g. display error message)
+      console.error(response.data.message);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
   return (
     <div className="modal">
